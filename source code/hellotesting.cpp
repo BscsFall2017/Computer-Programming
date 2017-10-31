25c76502f72d27c76529e9e13d56d01f4c79da4a
BscsFall2017 <bscs1212@gmail.com>
2017-10-31 16:00:07 +0500

    Code

---
+++ source code/hellotesting.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	
+	cout<<" HELLO HELLO !";
+	int a,b;
+	cout<<" O HELLO THIS PROGRAM TAKES TWO NUMBERS AND DOES SOME OPERATION";
+	
+	
+cout<<endl;
+
+cout<<" please enter your First Number:";
+cin>> a;
+cout<<" please enter your Second Number:";
+cin>>b;
+
+
+	int sum=0;
+	sum = a+b;
+	cout << "This program gives us sum of two integers"<<endl;
+	cout<<"The sum is:";
+cout<<sum;
+cout<<endl;
+cout<<"Thats it!";
+}
